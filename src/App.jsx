70d6b434c68da758f7d2e43ff621fdee616733c3
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-11-20 09:42:54 +0100

    xmas snow added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,33 @@ import Experience from './components/Experience.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
 import Portfolio from './components/Portfolio.jsx';
-
+import { useState } from 'react';
+import Snowfall from 'react-snowfall';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTree } from '@fortawesome/free-solid-svg-icons';
+import "./App.css"
 
 
 
 
 function App() {
-  
+  const [showSnow, setShowSnow] = useState(false);
+
+  const toggleSnow = () => {
+    setShowSnow(!showSnow);
+  }
 
   return (
     <>
+    <div className="switcher-container">
+       <label className="switch">  
+        <input type="checkbox" onChange={toggleSnow} checked={showSnow} />
+        <span className="slider round"></span>
+      </label>
+      🎄🎅 Switch for Xmas Edition🎄🎅
+      </div>
+     
+
       <Header  />
       <Topbar />
       <Intro   />
@@ -22,6 +39,11 @@ function App() {
       <Portfolio  />
       <Contact  />
       <Footer />
+      {showSnow && (
+        <div className="snowfall-container">
+          <Snowfall snowflakeCount={100} />
+        </div>
+        )}
 
     </>
   )
